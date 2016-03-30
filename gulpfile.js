83c0e637ff061d5d9b90ffaca3f172b03beae3a6
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-03-30 12:33:53 -0700

    Add coveralls, add engines/repo fields to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 'use strict';
 
+var coveralls = require('gulp-coveralls');
 var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
+
+gulp.task('coveralls', function () {
+	gulp.src('coverage/**/lcov.info')
+		.pipe(coveralls());
+});
 
 gulp.task('pre-test', function () {
 	return gulp.src(['index.js', 'lib/*.js'])
