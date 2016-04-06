bbcd6a35a15c45339ecb16e5ada74e0819baff51
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-04-06 14:44:56 -0700

    Add test for hooking tasks created in extension function

--- test/index.js
+++ test/index.js
@@ -300,6 +300,48 @@ describe('Lifray Plugin Tasks', function() {
 			});
 		});
 
+		it('should register hooks for extension tasks', function(done) {
+			gulp = new Gulp();
+
+			var hookSpy = sinon.spy();
+
+			var hookFn = function(gulp) {
+				gulp.hook('before:plugin:war', function(cb) {
+					hookSpy('before:plugin:war');
+
+					cb();
+				});
+
+				gulp.hook('after:my-custom:task', function(cb) {
+					hookSpy('after:my-custom:task');
+
+					cb();
+				});
+			};
+
+			registerTasks({
+				extensions: function(options) {
+					options.gulp.task('my-custom:task', function(cb) {
+						hookSpy('my-custom:task');
+
+						cb();
+					});
+				},
+				gulp: gulp,
+				hookFn: hookFn
+			});
+
+			runSequence = require('run-sequence').use(gulp);
+
+			runSequence('plugin:war', 'my-custom:task', function() {
+				assert(hookSpy.getCall(0).calledWith('before:plugin:war'));
+				assert(hookSpy.getCall(1).calledWith('my-custom:task'));
+				assert(hookSpy.getCall(2).calledWith('after:my-custom:task'));
+
+				done();
+			});
+		});
+
 		it('should overwrite task', function(done) {
 			gulp = new Gulp();
 
