96651da15ea6b6cd712db6b92f65e70e2c86727c
Zak Thompson <zak.thompson@echelon.org>
2017-02-08 11:45:03 -0600

    Add new parameterized init task
    Add a new gulp task 'plugin:initParameterized' to allow for the configuration json file to be created by passing in command line agruments instead of requiring a prompt.  
    
    This will be useful, as it will make it much easier to run the init task in bash or powershell scripts.  Instead of having to prompt the user for input each time the init task is ran, the appServerPath and url can now be stored in variables and passed to the parameterized init.  
    
    I would be willing to write unit tests if required to be merged, however, I'm not sure what the best way to mock receiving command line arguments would be, since I'm currently reading directly from yargs.

--- tasks/init.js
+++ tasks/init.js
@@ -2,8 +2,11 @@
 
 var InitPrompt = require('../lib/init_prompt');
 var path = require('path');
+var argv = require('yargs').argv;
+var gutil = require('gulp-util');
 
 var TASK_PLUGIN_INIT = 'plugin:init';
+var TASK_PLUGIN_INIT_PARAMETERIZED = 'plugin:initParameterized';
 
 module.exports = function(options) {
 	var gulp = options.gulp;
@@ -13,8 +16,26 @@ module.exports = function(options) {
 	gulp.task(TASK_PLUGIN_INIT, function(cb) {
 		new InitPrompt({
 			appServerPathDefault: store.get('appServerPath') || path.join(path.dirname(process.cwd()), 'tomcat'),
-			store: store
+			store: store,
+			shouldPrompt: true
 		}, cb);
+	});
+
+	gulp.task(TASK_PLUGIN_INIT_PARAMETERIZED, function(cb) {
+		var appServerPath = argv.appServerPath || false;
+		var url = argv.url || false;
+		
+		if(appServerPath && url){
+			new InitPrompt({
+				appServerPath: appServerPath,
+				url: url,
+				store: store,
+				shouldPrompt: false
+			}, cb);
+		} else {
+			gutil.log("Missing Parameters.  Please run 'gulp plugin:initParameterized' with --appServerPath and --url parameters");
+			process.exit();
+		}
 	});
 
 	gulp.task('init', [TASK_PLUGIN_INIT]);
